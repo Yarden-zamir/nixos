a5d0acbda88e03fd964e028376c5d16d593fa8c1
Yarden zamir <dev@yarden-zamir.com>
2024-08-31 17:06:08 +0300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -127,16 +127,6 @@
                 sha256 = "1b4pksrc573aklk71dn2zikiymsvq19bgvamrdffpf7azpq6kxl2";
               };
             }
-            {
-            # will source zsh-autosuggestions.plugin.zsh
-            name = "zsh-autosuggestions";
-            src = pkgs.fetchFromGitHub {
-              owner = "zsh-users";
-              repo = "zsh-autosuggestions";
-              rev = "v0.4.0";
-              sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
-            };
-            }
           ];
       };};
 
